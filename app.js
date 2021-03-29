49565477e252d455d5471aa33d87afbef219bb2f
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-03-28 22:14:46 -0700

    modifies axios post endpoints

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 //instantiate app and configure middleware
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // app.get('/createimagecid', (req, res) => {
 //   res.send("This will appear if the server works")
