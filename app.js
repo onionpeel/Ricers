fa017dc73dfb3458f163a20ceb85fdc7cde8e3af
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-03-28 20:27:20 -0700

    adds cors middleware

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/test', (req, res) => {
   res.send("This will appear if the server works")
 });
